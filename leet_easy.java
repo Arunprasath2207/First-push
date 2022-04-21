0a3455fd33328b0e27cf3756e8cc010bdd874ee3
Arunprasath2207 <84843162+Arunprasath2207@users.noreply.github.com>
2022-04-21 19:33:21 +0530

    Add files via upload

---
+++ leet_easy.java
@@ -0,0 +1,12 @@
+class leet_easy
+{
+    public int[] buildArray(int[] nums) 
+    {
+        int[] n = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            n[i] = nums[nums[i]];
+        }
+        return n;
+    }
+}
