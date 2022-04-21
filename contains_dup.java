a5d54a5287871cc04c1e841b71e73db38e1322b3
Arunprasath2207 <84843162+Arunprasath2207@users.noreply.github.com>
2022-04-21 20:37:42 +0530

    Add files via upload

---
+++ contains_dup.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class contains_dup
+{
+    public boolean containsDuplicate(int[] nums) 
+    {
+       Map<Integer,Integer> map = new HashMap();
+       for(int i=0;i<nums.length;i++)
+       {
+           if(map.containsKey(nums[i]))
+              return true;
+           map.put(nums[i],0);
+       }
+        return false;
+      
+    }
+}
